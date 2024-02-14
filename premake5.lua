7e3624a96f666c9bc74e77b0317d3267fb68ac6c
Qrikko <qrikko@gmail.com>
2024-02-14 20:14:24 +0100

    updates to premake

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,5 @@
 project "imgui"
+--	kind "SharedLib"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++17"
@@ -7,22 +8,36 @@ project "imgui"
 	targetdir ("bin/" .. outputdir.. "/")
 	objdir("bin-inc/" .. outputdir .. "/%{prj.name}")
 
+	includedirs {
+		"./",
+		"./backends/"
+	}
+
+	symbols "on"
+	links { "vulkan" }
+
+	linkoptions { "-fPIC", "-O0", "-lstdc++" }
+	buildoptions { "-fPIC", "-O0", "-g", "-fno-exceptions" }
+
 	files {
 		"imconfig.h",
 		"imgui.h",
 		"imgui.cpp",
+		"imgui_tables.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
 		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+		"./backends/imgui_impl_vulkan.cpp"
 	}
 
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
+
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
